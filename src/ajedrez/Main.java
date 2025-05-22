df9ade2da32f8e3776ac7d39f97faa9467055578
joseCalfueque <j.calfueque02@ufromail.cl>
2025-05-22 14:07:16 -0400

    main

--- src/ajedrez/Main.java
+++ src/ajedrez/Main.java
@@ -1,10 +1,20 @@
 package ajedrez;
 
-
+import ajedrez.modelo.Tablero;
 import ajedrez.vista.TableroGUI;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new TableroGUI();
+        SwingUtilities.invokeLater(() -> {
+            Tablero tablero = new Tablero();
+            JFrame frame = new JFrame("Ajedrez - Proyecto UFRO");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new TableroGUI(tablero));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
